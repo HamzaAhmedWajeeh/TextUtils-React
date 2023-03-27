55ebb1c7b9a2590db5c04d269d67d07b715dc973
HamzaAhmedWajeeh <98827559+HamzaAhmedWajeeh@users.noreply.github.com>
2023-03-27 21:30:54 +0500

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+let a = "Hamza";
+let b = "Talha";
+let c = "Zaid";
+let d = "Anas";
+
+export default a;
+export {b};
+export {c};
+export {d};
+
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import h, {b,c,d} from './module1.mjs'
+
+console.log(h);
+console.log(b);
+console.log(c);
+console.log(d);
